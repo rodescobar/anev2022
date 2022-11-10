e6d716d91a1c59c4e58b879ea9441bb6ffa4414e
Rodrigo Escobar <rodescobar@gmail.com>
2022-11-09 22:00:04 -0300

    aula13

--- Aula13/index.js
+++ Aula13/index.js
@@ -3,10 +3,31 @@ var app = express()
 
 var Cliente = require("./model/cliente")
 
+//URL : 127.0.0.1
+//Endpoint: cliente
+//URL: 127.0.0.1/cliente
+   /*
+        GET => Lista clientes cadastrados no banco.
+        POST => Inclusão de cliente
+        PUT => Alteracao de Cliente
+        DELETE => Exclusao de cliente
+   */
 
+app.get("/cliente", function(req, res) {
+    res.send("Lista de clientes")
+})
 
+app.post("/cliente", function(req, res) {
+    res.send("Inclusao de cliente")
+})
 
+app.put("/cliente", function(req, res) {
+    res.send("Alteracao de cliente")
+})
 
+app.delete("/cliente", function(req, res) {
+    res.send("Exclusao de cliente")
+})
 
 app.listen(80, function(req, res) {
     console.log("Servidor Online")
